71d4473e243d49dda61a95e1737699d4d2f094ff
Urszula Ciaputa <urszula.ciaputa@xhtmlized.com>
2015-02-20 17:37:17 +0100

    Start of section extraction

--- app/templates/grunt/update-main.js
+++ app/templates/grunt/update-main.js
@@ -1,22 +1,78 @@
 var async = require('async');
-var numCPUs = require('os').cpus().length || 1;
+var path = require('path');
+var fs = require('fs');
 
 module.exports = function (grunt) {
-'use strict';
+  'use strict';
 
-  grunt.registerMultiTask('update-main', 'Updates main.scss', function () {
-    async.eachLimit(this.files, numCPUs, function (file, next) {
-      var src = file.src[0];
-
-      if (typeof src !== 'string') {
-        src = file.orig.src[0];
-      }
-
-      console.log(this.files, src, next);
-    });
+  grunt.config('updatemain', {
+    build: {
+      src: '<%%= xh.src %>/scss/**/*.scss'
+    },
+    options: {
+      main: '<%%= xh.src %>/scss/main.scss'
+    }
   });
 
-  grunt.config('update-main', {
-    dist: {}
+  grunt.registerMultiTask('updatemain', 'Updates main.scss/less after new component file is added', function () {
+    var self = this;
+    var done = this.async();
+    var mainStylesheet = this.options().main;
+
+    async.eachLimit(this.files, 4, function (file, next) {
+      var src = file.src;
+
+      if (!src) {
+        return next();
+      }
+
+      for (var i = 0; i < src.length; i++) {
+        if (src[i] === mainStylesheet) {
+          return next();
+        }
+        addFileToMain.bind(self)(src[i]);
+      }
+
+      next();
+    }, done);
+
+    function addFileToMain (file) {
+      var root = path.join(grunt.config('xh').src, 'scss') + path.sep;
+      var importFile = file.replace(root, '').replace(path.sep + '_', path.sep).replace(/\.(scss|less)$/, '');
+      var mainStylesheet = this.options().main;
+
+      fs.readFile(mainStylesheet, 'utf-8', 'r+', function (err, data) {
+        if (err) throw err;
+        writeToMainStylesheet(data, importFile);
+      });
+    }
+
+    function writeToMainStylesheet (data, importFile) {
+      var section = (importFile.split(path.sep) || ['default'])[0];
+      var sectionComment = '// @@' + section;
+      var importStylesheet = '@import "' + importFile + '";\n';
+      var sectionCommentPosition = data.indexOf(sectionComment);
+      var isImportStylesheetPresent = data.indexOf(importStylesheet) !== -1;
+
+      if (sectionCommentPosition !== -1 && !isImportStylesheetPresent) {
+        writeInsideMain();
+      }
+      else if (data.indexOf(sectionComment) === -1) {
+        appendSectionToMain();
+      }
+
+      function writeInsideMain () {
+        var newValue = data.slice(0, sectionCommentPosition) + importStylesheet + data.slice(sectionCommentPosition);
+        fs.writeFile(mainStylesheet, newValue, 'utf-8', function (err) {
+          if (err) throw err;
+        });
+      }
+
+      function appendSectionToMain () {
+        fs.appendFile(mainStylesheet, '\n' + importStylesheet + sectionComment, 'utf-8', function (err) {
+          if (err) throw err;
+        });
+      }
+    }
   });
 };
